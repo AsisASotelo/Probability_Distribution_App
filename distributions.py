880490bcb6b33cd30102f4eaac7c4d75856391d2
Mark I <marki@Asiss-MacBook-Pro.local>
2020-02-26 14:35:27 -0800

    Uploaded Base Python Code

---
+++ distributions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# distributions.py
+# Asis Sotelo
+# 02/21/2020
+
+"""
+
+Program designed to graph various probability distributions
+
+
+"""
+
+import sys
+import scipy
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Ralyeigh Distribution
+
+class Ralyeigh_Gen:
+
+            
